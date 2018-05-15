4815c058ce177bcca67c26d8036f70409cc8f57f
Dilschat <dilschat@yandex.ru>
2018-05-15 18:14:25 +0300

    prettify output

--- src/main/java/com/example/simple_biosamples_client/DAOs/BiosamplesAccessPoint.java
+++ src/main/java/com/example/simple_biosamples_client/DAOs/BiosamplesAccessPoint.java
@@ -6,13 +6,14 @@ import org.springframework.hateoas.Resource;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.biosamples.client.BioSamplesClient;
 import uk.ac.ebi.biosamples.model.Sample;
+import uk.ac.ebi.biosamples.model.filter.Filter;
+import uk.ac.ebi.biosamples.service.FilterBuilder;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 @Component
 @Scope("prototype")
-public class BiosamplesAccessPoint implements AccessPoint {
+public class BiosamplesAccessPoint {
     private BioSamplesClient client;
 
     @Autowired
@@ -25,5 +26,18 @@ public class BiosamplesAccessPoint implements AccessPoint {
         Resource<Sample> sampleResource = optionalResource.get();
         return sampleResource.getContent();
     }
+
+
+    public Iterable<Resource<Sample>> getFilteredSamplesByString(String text, List<String> rawFilters) {
+        Collection<Filter> filters = new LinkedList<>();
+        if (rawFilters != null) {
+            for (String filter : rawFilters) {
+                filters.add(FilterBuilder.create().buildFromString(filter));
+            }
+        }
+
+        return client.fetchSampleResourceAll(text, filters);
+    }
+
 }
 
--- src/main/java/com/example/simple_biosamples_client/controllers/SimpleController.java
+++ src/main/java/com/example/simple_biosamples_client/controllers/SimpleController.java
@@ -1,22 +1,22 @@
 package com.example.simple_biosamples_client.controllers;
 
-import com.example.simple_biosamples_client.DAOs.AccessPoint;
+import com.example.simple_biosamples_client.DAOs.BiosamplesAccessPoint;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.*;
 import uk.ac.ebi.biosamples.model.Sample;
 
-import java.io.IOException;
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
 public class SimpleController {
 
     @Autowired
-    protected AccessPoint accessPoint;
+    protected BiosamplesAccessPoint accessPoint;
 
 
     @RequestMapping("/helloworld")
@@ -25,9 +25,16 @@ public class SimpleController {
     }
 
     @RequestMapping(value="/getSample/{sampleID}", method=RequestMethod.GET)
-    String getSample(@PathVariable String sampleID) throws IOException {
+    Sample getSample(@PathVariable String sampleID) {
         Sample biosample = accessPoint.getSample(sampleID);
-        return biosample.toString();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return biosample;
+    }
+
+    @RequestMapping("/search")
+    Iterable<Resource<Sample>> searchAndFilter(@RequestParam("text") String text, @RequestParam(value = "filter", required = false) List<String> filters) {
+        return accessPoint.getFilteredSamplesByString(text, filters);
     }
 
 }
