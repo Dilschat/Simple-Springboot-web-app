99b5a7a63fb726cb4276cf97dbcc0b11cac02229
Dilschat <dilschat@yandex.ru>
2018-05-30 22:19:25 +0300

    GeolocationHelper test implemented (Not complete.Geolocation mapping will be changed)

--- src/test/java/com/example/simple_biosamples_client/OLSDataRetreiverTest.java
+++ src/test/java/com/example/simple_biosamples_client/OLSDataRetreiverTest.java
@@ -1,4 +1,26 @@
 package com.example.simple_biosamples_client;
 
+import com.example.simple_biosamples_client.services.utils.OLSDataRetriever;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OLSDataRetreiverTest {
+
+    @Test
+    public void id_retrieving_test() {
+        OLSDataRetriever retriever = new OLSDataRetriever();
+        retriever.readJsonFromUrl("http://purl.obolibrary.org/obo/NCBITaxon_9606");
+        String expected_id = "NCBITaxon:9606";
+        String actual_id = retriever.StringGetOntologyTermId();
+        assertEquals(actual_id, expected_id);
+    }
+
+    @Test
+    public void label_retreiving_test() {
+        OLSDataRetriever retriever = new OLSDataRetriever();
+        retriever.readJsonFromUrl("http://purl.obolibrary.org/obo/NCBITaxon_9606");
+        String expected_label = "Homo sapiens";
+        String actual_label = retriever.StringGetOntologyTermLabel();
+    }
 }
